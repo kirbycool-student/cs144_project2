5e276790badeaba83b2618aed2b286ce088031e6
Kirby Cool <kirbydcool@gmail.com>
2013-10-26 19:40:28 -0700

    finished 3

--- queries.sql
+++ queries.sql
@@ -1,2 +1,3 @@
 select count(*) from User;
-select * from User u join Item i on u.UserId = i.Seller where u.location = 'New York'; 
+select count(*) from (select distinct u.UserId from User u join Item i on u.UserId = i.Seller where u.location = 'New York' collate utf8_bin) as t;
+select count(*) from (select count(*) as count from Item i join ItemCategory c on i.ItemId = c.ItemId group by i.itemId) as t where t.count = 4;
